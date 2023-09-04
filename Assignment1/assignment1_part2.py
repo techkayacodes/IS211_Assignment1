3463b658c2eb52b6d4b3dc087b2df4f720a53aad
Tech Kaya <techkayacodes@gmail.com>
2023-09-03 21:50:06 -0400

    Class Book with 2 attributes author and title made

--- Assignment1/assignment1_part2.py
+++ Assignment1/assignment1_part2.py
@@ -1,19 +1,18 @@
+# Create a class called Book
 class Book:
-    author = ""
-    title = ""
-
-    def __init__(self, author, title):
+    # Initialize attributes author and title to blank strings
+    def __init__(self, author="", title=""):
         self.author = author
         self.title = title
 
+    # A function to display the book's information
     def display(self):
-        print("\""+self.title+", written by "+self.author+"\"")
+        print(f"{self.title}, written by {self.author}")
 
-object1 = Book("John Steinbeck", "Of Mice and Men")
-object1.display()
+# Instantiate two objects from the Book class
+book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+book2 = Book("Walter Scott", "Ivanhoe: A Romance")
 
-object2 = Book("Haper Lee", "To Kill a Mockingbird")
-object2.display()
-
-if __name__ == "__main__":
-    pass
+# Print both objects by calling their display() functions
+book1.display()
+book2.display()
